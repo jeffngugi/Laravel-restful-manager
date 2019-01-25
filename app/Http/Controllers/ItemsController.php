5ac1a66db5546f67d66581fb1317d726acbb0ee9
jeffngugi <ngugimwathi254@gmail.com>
2019-01-25 11:03:57 +0300

    Finalised by creating update and delete functions for the RESTful application

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -19,15 +19,7 @@ class ItemsController extends Controller
         return response()->json($items);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+ 
 
     /**
      * Store a newly created resource in storage.
@@ -69,16 +61,7 @@ class ItemsController extends Controller
 
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+    
 
     /**
      * Update the specified resource in storage.
@@ -89,7 +72,22 @@ class ItemsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator = Validator::make($request->all (),[
+            'text' => 'required'
+        ]);
+
+        if($validator->fails()){
+            $response = array('response' => $validator->messages(), 'Success'=>false);
+            return $response;
+        }else{
+            $item = Item::find($id);
+            $item->text = $request->input('text');
+            $item ->body = $request->input('body');
+            $item->save();
+
+            return response()->json($item);
+
+        }
     }
 
     /**
@@ -100,6 +98,6 @@ class ItemsController extends Controller
      */
     public function destroy($id)
     {
-        //
+        
     }
 }
